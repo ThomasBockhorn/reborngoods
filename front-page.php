f5fb3c0e4ddd30424f952b67c3d621c460f68d9d
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2019-01-05 12:21:51 -0600

    Updated the rotating featured image

--- front-page.php
+++ front-page.php
@@ -12,10 +12,10 @@
   $loop = new WP_Query($args);
   while ($loop->have_posts()) : $loop->the_post();
   global $product; ?>
-    <div class="dvThumb col-xs-4 col-sm-3 col-md-3 profiler-select profiler<?php echo the_title(); ?>" data-profile="<?php echo $loop->post->ID; ?>">
+    <div class="col-lg-12 text-center" data-profile="<?php echo $loop->post->ID; ?>">
         <?php $featured_image = wp_get_attachment_image_src(get_post_thumbnail_id($loop->post->ID)); ?>
         <?php if ($featured_image) { ?>
-        <img src="<?php echo $featured_image[0]; ?>" data-id="<?php echo $loop->post->ID; ?>">
+        <img id="featured-image" src="<?php echo $featured_image[0]; ?>" data-id="<?php echo $loop->post->ID; ?>">
         <?php 
       } ?>
     </div>
