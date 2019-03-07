ea388d25f4d27ad1726434bb77655be894347e2c
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2019-03-07 15:44:27 -0600

    fixed header

--- header.php
+++ header.php
@@ -10,28 +10,19 @@
 <body <?php body_class(); ?>>
 <header>
 <!--Header for reborngoods theme-->
-<nav class="navbar navbar-expand-lg">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a id="home" class="navbar-brand" href="<?php echo site_url('/home') ?>"><?php echo get_bloginfo('name') ?></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fas fa-bars"></i>
-      </button>
-    </div>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <div class="container">
-        <div class="row">
-          <form class="menu col-md-12 col-sm-12" id="navigation_menu">
-            <?php
-            wp_nav_menu(array(
-              'theme_location' => 'headerMenuLocation',
-              'menu_class' => 'navbar-nav',
-            ));
-            ?>
-          </form>
-        </div>
-      </div>  
-    </div>   
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="<?php echo site_url('/') ?>"><?php echo get_bloginfo('name') ?></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <?php
+    wp_nav_menu(array(
+      'theme_location' => 'headerMenuLocation',
+      'menu_class' => 'navbar-nav',
+    ));
+    ?>
   </div>
 </nav>
 </header>
