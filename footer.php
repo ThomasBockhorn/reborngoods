1f011a1c88540c259c872267d3ba67d2b2015c7f
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2019-03-07 10:54:19 -0600

    Updated jumbotron and footer

--- footer.php
+++ footer.php
@@ -3,21 +3,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="name-address col-lg-4">
-                <h1><?php bloginfo(); ?></h1>
-                <p><?php 
-                //Split the country and state
-                    $split_country = explode(":", get_option('woocommerce_default_country'));
-
-                //Country and state separated
-                    $store_country = $split_country[0];
-                    $store_state = $split_country[1];
-
-                    echo get_option('woocommerce_store_address') . "<br />";
-                    echo (get_option('woocommerce_store_address_2')) ? get_option('woocommerce_store_address_2') . "<br />" : '';
-                    echo get_option('woocommerce_store_city') . ', ' . $store_state . ' ' . get_option('woocommerce_store_postcode') . "<br />";
-                    echo $store_country . "<br />";
-                    echo get_option('admin_email');
-                    ?></p>
+                <h1><?php bloginfo('name'); ?></h1>
             </div>
             <div class="footer-menu col-lg-4">
             <?php
--- front-page.php
+++ front-page.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
-<div class="jumbotron container" id="landing_page_jumbotron">
+<div class="jumbotron container text-center" id="landing_page_jumbotron">
   <h1 class="display-4" id="heading">Reborn Goods</h1>
   <p class="lead" id="heading">Vintage Clothing and Accessories</p>
   <?php
   $args = array(
     'post_type' => 'product',
-    'posts_per_page' => 1,
+    'posts_per_page' => 3,
     'orderby' => 'rand'
-  );
-
-  $loop = new WP_Query($args);
-  while ($loop->have_posts()) : $loop->the_post();
-  global $product; ?>
-    <div class="col-lg-12 text-center" data-profile="<?php echo $loop->post->ID; ?>">
+  ); ?>
+<div class="imageSet">
+<?php $loop = new WP_Query($args);
+while ($loop->have_posts()) : $loop->the_post();
+global $product; ?>
+    <div class="jumboImage" data-profile="<?php echo $loop->post->ID; ?>">
         <?php $featured_image = wp_get_attachment_image_src(get_post_thumbnail_id($loop->post->ID)); ?>
         <?php if ($featured_image) { ?>
         <img class="img-responsive" id="featured-image" src="<?php echo $featured_image[0]; ?>" data-id="<?php echo $loop->post->ID; ?>">
@@ -20,5 +20,6 @@
       } ?>
     </div>
 <?php endwhile; ?>
+</div>
 </div>
 <?php get_footer(); ?>
